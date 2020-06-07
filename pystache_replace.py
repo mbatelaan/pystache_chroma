bcb79d1d4809a32978aa72cb18ba1c8d290d8e9b
mbatelaan <mischa.batelaan@adelaide.edu.au>
2020-06-07 16:58:55 +0930

    changed a lot of things, to allow for more options, the lambda can now be set for each momentum seperately, the different sink smearings are saved to seperate files and I added a few more parameters to the files

--- pystache_replace.py
+++ pystache_replace.py
@@ -8,7 +8,8 @@ print(conf)
 partials = json.load(open('partials.json','r'))
 parameters = json.load(open(parameterfile,'r')) #parameters_allq.json 
 
-file = open('input_template.xml.ms', 'r').read()
+#file = open('input_template.xml.ms', 'r').read()
+file = open('test_input_template.xml.ms', 'r').read()
 template = pystache.parse(file)
 ms_renderer = pystache.Renderer(partials = partials)
 def render_ms(template, obj):
