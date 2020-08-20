7d3e17d5a9ccbd6a47dc74d9583abf73c7fb99dd
mbatelaan <mischa.batelaan@adelaide.edu.au>
2020-08-20 11:03:01 +0930

    new parameter files

--- pystache_replace.py
+++ pystache_replace.py
@@ -2,16 +2,16 @@ import sys
 import pystache
 import json
 
-parameterfile = sys.argv[1]
-conf=parameterfile[11:27]
+parameterfile = sys.argv[1] # file containing the pystache parameters
+conf=parameterfile[11:27] # Extract kappa values
 print(conf)
-partials = json.load(open('partials.json','r'))
+partials = json.load(open('partials.json','r')) # File with partials for pystache
 parameters = json.load(open(parameterfile,'r')) #parameters_allq.json 
 
 #file = open('input_template.xml.ms', 'r').read()
-file = open('test_input_template.xml.ms', 'r').read()
+file = open('test_input_template.xml.ms', 'r').read() # Input template
 template = pystache.parse(file)
-ms_renderer = pystache.Renderer(partials = partials)
+ms_renderer = pystache.Renderer(partials = partials) # Apply the partials
 def render_ms(template, obj):
     return ms_renderer.render(template,obj)
 open('chromatemplate_'+conf+'.xml', 'w+').write(render_ms(template, parameters).replace('[[', '{{').replace(']]', '}}'))
