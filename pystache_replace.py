b1dddb7075b50f1788cff00b7b1742e348d3b781
mbatelaan <mischa.batelaan@adelaide.edu.au>
2021-02-26 12:07:16 +1030

    added b5p40kp120048kp119695.json file

--- pystache_replace.py
+++ pystache_replace.py
@@ -3,7 +3,12 @@ import pystache
 import json
 
 parameterfile = sys.argv[1] # file containing the pystache parameters
+
+#CVC partial:
+#partials = json.load(open('partials_CVC.json','r')) # File with partials for pystache
+#local current partial
 partials = json.load(open('partials.json','r')) # File with partials for pystache
+
 parameters = json.load(open(parameterfile,'r')) #parameters_allq.json 
 print(parameters['kappas'][0]['kappa'])
 
@@ -16,13 +21,18 @@ outname = kappastr
 
 # Make a string for the beta value if it exists in the dictionary
 if 'beta' in parameters:
-    betastr = 'b'+str(parameters['beta'])[0]+'p'+str(parameters['beta'])[2:]
+    betastr = 'b'+parameters['beta'][0]+'p'+parameters['beta'][2:]
+    #betastr = 'b'+str(parameters['beta'])[0]+'p'+str(parameters['beta'])[2:]
     outname = betastr + outname
 outname = 'chromatemplate_' + outname + '.xml'
 print(outname)
 
-#file = open('test_input_template.xml.ms', 'r').read()
-file = open('input_template_CVC.xml.ms', 'r').read() # Input template
+# local current template
+file = open('test_input_template.xml.ms', 'r').read()
+
+#CVC input template
+#file = open('input_template_CVC.xml.ms', 'r').read() # Input template
+
 template = pystache.parse(file)
 ms_renderer = pystache.Renderer(partials = partials) # Apply the partials
 def render_ms(template, obj):
